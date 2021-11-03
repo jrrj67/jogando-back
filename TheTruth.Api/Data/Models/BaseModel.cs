7cfd7fa7336d5ec6614db95bc9fb91a39c349a2a
José Ricardo da Rocha Junior <ricardojunior.67@hotmail.com>
2021-11-03 19:15:51 -0300

    Fresh start.

--- TheTruth.Api/Data/Models/BaseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TheTruth.Api.Data.Models
-{
-    public abstract class BaseModel
-    {
-        public int Id { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-    }
-}
--- TheTruth.Api/Data/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TheTruth.Api.Data.Models
-{
-    public class User : BaseModel
-    {
-        public string Name { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-    }
-}
