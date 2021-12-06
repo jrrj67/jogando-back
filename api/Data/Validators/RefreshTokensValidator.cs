e40cb393bff59b3710a84f515edb736a974e0ac8
Ricardo Rocha <ricardojunior.6767@gmail.com>
2021-12-06 00:12:08 -0300

    Removed prop.

--- api/Data/Validators/RefreshTokensValidator.cs
+++ api/Data/Validators/RefreshTokensValidator.cs
@@ -1,14 +1,12 @@
 ﻿using FluentValidation;
 using JogandoBack.API.Data.Models.Requests;
 using JogandoBack.API.Data.Services.Token;
-using Microsoft.Extensions.Configuration;
 
 namespace JogandoBack.API.Data.Validators
 {
     public class RefreshTokensValidator : AbstractValidator<RefreshTokensBaseRequest>
     {
         private readonly IRefreshTokenService _refreshTokenService;
-        public IConfiguration Configuration { get; set; }
 
         public RefreshTokensValidator(IRefreshTokenService refreshTokenService)
         {
